dda2d681abf1732ba8a252fc2e07af8759e61229
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-05-31 14:46:20 -0500

    included code for app.js

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const {getCityInfo, getJobs} = require('./util.js')
+console.log(getCityInfo, getJobs)
+
+app.use(express.static('public'))
+
+app.get('/api/city/:city', async(req, res) => {
+    const cityInfo = await getCityInfo(req.params.city)
+    const jobs = await getJobs(req.params.city)
+    if (!(cityInfo || jobs)){
+      return res.status(404).json({error: "Iformation not found"})
+
+    }
+    res.json ({cityInfo, jobs})
+    console.log('get info')
+   
+})
 // TODO: import the getCityInfo and getJobs functions from util.js
-
 // TODO: Statically serve the public folder
-
-// TODO: declare the GET route /api/city/:city
+// TODO: declare the GET route /api/city/:city   
 // This endpoint should call getCityInfo and getJobs and return
 // the result as JSON.
 // The returned JSON object should have two keys:
